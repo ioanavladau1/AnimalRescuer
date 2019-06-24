e3814855a5af08810a5322fee2843b70878a582b
ioana.cora <ioana.cora@casomes.ro>
2019-06-24 20:45:27 +0300

    gitignore

--- src/main/java/org/fasttrackit/Adopter.java
+++ src/main/java/org/fasttrackit/Adopter.java
@@ -5,3 +5,4 @@ public class Adopter {
     String name;
     double money;
 }
+
--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,25 +1,32 @@
 package org.fasttrackit;
 
+import java.time.LocalDate;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Animal animal = new Animal ();
-        animal.name= "Soso";
-        animal.age= 5;
-        animal.health 8;
-        animal.hungerLevel= 6;
-        animal.mindset= 7;
-        animal.animalFood= "Pedigree";
-        animal.activities= "play";
+public class App {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.name = "Soso";
+        animal.age = 5;
+        animal.health = 8;
+        animal.hungerLevel = 6;
+        animal.mindset = 7;
+        animal.animalFood = "Pedigree";
+        animal.activities = "play";
 
-        Adopter adopter= new Adopter();
-        adopter.name= "Ana";
-        adopter.money= 420;
+        Adopter adopter = new Adopter();
+        adopter.name = "Ana";
+        adopter.money = 420;
+
+        AnimalFood animalFood = new AnimalFood();
+        animalFood.foodname = "Pedigree";
+        animalFood.price = 20;
+        animalFood.quantity = 10;
+        animalFood.stock = 25;
+        animalFood.expirationDate = LocalDate.of(2020,10,25);
 
 
     }
